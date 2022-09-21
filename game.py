e9bdd3d80e510e080373be64898ff5ffb9662fc2
druciits <114051197+druciits@users.noreply.github.com>
2022-09-21 10:18:14 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,21 +1,23 @@
 import random
 x = ['rock', 'paper', 'scissors']
 
-print('ROCK, PAPER, SCISSORS')
-print("Let's begin!")
+while(True):
+    print('ROCK, PAPER, SCISSORS')
+    print("Let's begin!")
 
-p1 = input('Enter your choice:')
-p2 = random.choice(x)
+    p1 = input('Enter your choice:')
+    p2 = random.choice(x)
 
-print(f'You: {p1}   vs.   Computer: {p2}')
+    print(f'You:{p1}   vs.   Computer:{p2}')
 
-if p1 == p2:
-    print('Draw!')
-elif p1 == 'rock' and p2 == 'paper':
-    print('Computer Wins!')
-elif p1 == 'paper' and p2 == 'scissors':
-    print('Computer Wins!')
-elif p1 == 'scissors' and p2 == 'rock':
-    print('Computer Wins!')
-else:
-    print('You Win!')
+    if p1 == p2:
+        print('Draw!')
+    elif p1 == 'rock' and p2 == 'paper':
+        print('Computer Wins!')
+    elif p1 == 'paper' and p2 == 'scissors':
+        print('Computer Wins!')
+    elif p1 == 'scissors' and p2 == 'rock':
+        print('Computer Wins!')
+    else:
+        print('You Win!')
+    
